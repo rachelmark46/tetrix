cdcc4878d7b8d9572d999606dcc8b47e01dfdc12
rachelmark46 <86923379+rachelmark46@users.noreply.github.com>
2024-08-23 16:14:21 +0530

    Released version

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:tetrix/gamer/gamer.dart';
+import 'package:tetrix/material/audios.dart';
+import 'package:tetrix/panel/page_portrait.dart';
+import 'gamer/keyboard.dart';
+
+void main() => runApp(MyApp());
+
+
+
+final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TETRIS',
+      // localizationsDelegates: [
+      //   S.delegate,
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate
+      // ],
+      navigatorObservers: [routeObserver],
+      //supportedLocales: S.delegate.supportedLocales,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Sound(child: Game(child: KeyboardController(child: _HomePage()))),
+      ),
+    );
+  }
+}
+
+const SCREEN_BORDER_WIDTH = 3.0;
+
+const BACKGROUND_COLOR = const Color(0xffefcc19);
+
+class _HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //only Android/iOS support land mode
+    bool land = MediaQuery.of(context).orientation == Orientation.landscape;
+    return land ? PageLand() : PagePortrait();
+  }
+}
---
+++ lib/splash_screen_page.dart
@@ -0,0 +1,58 @@
+//Black screen that appears as soon as user opens the app with the logo
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({Key? key}) : super(key: key);
+
+  @override
+  SplashScreenPageState createState() => SplashScreenPageState();
+}
+
+class SplashScreenPageState extends State<SplashScreenPage> {
+
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => MyApp()),
+      );
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // Background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo image
+            Image.asset(
+              'assets/images/PuzzelPixelStudioLaunch.png',
+              width: 200, // Adjust width as needed
+            ),
+            SizedBox(height: 24),
+                    Text(
+                      '\nTETRIS ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0,
+                          color: Colors.white),
+                    ),
+            SizedBox(height: 24), // Spacer
+            // Loading text
+            Text(
+              'Rachel Mark',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+ }
